6d5d09c71756ce4955d40d59ee14fab123af5882
huiji072 <fmti0720@gmail.com>
2022-05-12 11:26:14 +0900

    3-1.py 'law_of_large_numbers'

---
+++ Greedy/law_of_large_numbers.py
@@ -0,0 +1,12 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split())) #n개의 수를 공백으로 구분하여 입력받기
+sum = 0
+
+data.sort(reverse=True) #내림차순으로 정렬
+max = data[0]
+min = data[1]
+
+sum += (m % k) * min
+sum += (m -(m%k)) * max
+
+print(sum)
