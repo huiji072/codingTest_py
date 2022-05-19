c99b5aad08174089bbc46eb5186062e5479bc21a
huiji072 <fmti0720@gmail.com>
2022-05-19 15:33:04 +0900

    Implementation - ch12 - Q8

--- Greedy/LostParenthesis.py
+++
@@ -1,5 +0,0 @@
-arr = input().split('-')
-#10+20-30+40
-
-print(arr)
-
--- Implementation/StringRearrangement.py
+++ Implementation/StringRearrangement.py
@@ -1,19 +1,21 @@
 import re
 from string import digits
-
+#K1KA5CB7
 s = input()
 
 # 숫자만 추출
 numbers = re.findall(r'\d', s)
+
 # 숫자의 합
 sum = 0
 for i in range(len(numbers)):
     sum += int(numbers[i])
 sum = str(sum)
+
 # 숫자만 제거
-str = re.sub(r'[0-9]+', '', s)
+str = re.sub(r'[0-9]', '', s)
 str = list(str)
 
 # 오름차순 후 숫자와 결함
-s = "".join(s)
-print(s+sum)
+str.sort()
+print(''.join(s for s in str)+sum)
